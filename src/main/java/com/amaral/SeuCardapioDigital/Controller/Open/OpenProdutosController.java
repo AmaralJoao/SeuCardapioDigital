461537212f11b404782cb4d52f022d2744fe11b7
fed/Joao G Amaral <joaoguilherme.amaral05@gmail.com>
2025-07-27 00:14:39 -0300

    ajuste nomenclatura

--- src/main/java/com/amaral/SeuCardapioDigital/Controller/Open/OpenProdutosController.java
+++ src/main/java/com/amaral/SeuCardapioDigital/Controller/Open/OpenProdutosController.java
@@ -1,27 +1,18 @@
 package com.amaral.SeuCardapioDigital.Controller.Open;
 
-import com.amaral.SeuCardapioDigital.Dto.Request.ProdutoRequestDto;
 import com.amaral.SeuCardapioDigital.Dto.Response.ProdutoResponseDto;
 import com.amaral.SeuCardapioDigital.Service.ProdutoService;
 import com.amaral.SeuCardapioDigital.Utils.AuthUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.List;
-import java.util.UUID;
 
 @CrossOrigin(origins = "http://localhost:5500") // ou especifique a origem com "http://localhost:5500"
 @RestController
 @RequestMapping("/open/api/produto")
-public class AuthProdutosController {
+public class OpenProdutosController {
 
     @Autowired
     private ProdutoService produtoService;
